25a4cd361ce8ae3fb52abed6fc1a1ad75c6d4138
NickLin <nicklin9907@gmail.com>
2024-10-17 05:07:47 +0000

    Fix collapse animation

--- src/views/CreateCharacter.vue
+++ src/views/CreateCharacter.vue
@@ -1,11 +1,14 @@
 <script lang="ts" setup>
-import { ref, computed } from 'vue';
+import { ref, computed, nextTick } from 'vue';
 import Races from '../components/createCharacter/Races.vue';
 import Classes from '../components/createCharacter/Classes.vue';
 import { Categories } from '../assets/js/categories';
 
 
-const categories = ref<Categories>({});
+const categories = ref<{ [key: number]: Categories }>({});
+const currentCategories = computed(
+    () => currentStep.value in categories.value ? categories.value[currentStep.value] : {}
+)
 const categoryMapping: { [key: string]: string } = {
     race_traits: "种族特质",
     class_traits: "职业特质",
@@ -16,10 +19,17 @@ const stepCardTranslate = computed(() => {
     return `translate(${currentStep.value * -100}%, 0)`
 })
 const featureSelections = ref<{ [key: string]: Array<string> }>({});
+const categoryRefs = ref<Array<HTMLElement>>([]);
 
+function recordCategoryHeight() {
+    for (let featureRef of categoryRefs.value) {
+        featureRef.style.setProperty("height", `${featureRef.offsetHeight}px`)
+    }
+}
 
 function collapse(categoryName: string) {
-    categories.value[categoryName].collapse = !categories.value[categoryName].collapse
+    categories.value[currentStep.value][categoryName].collapse
+        = !categories.value[currentStep.value][categoryName].collapse
 }
 
 function updateCategories(categories_data: Categories) {
@@ -31,7 +41,8 @@ function updateCategories(categories_data: Categories) {
             }
         }
     }
-    categories.value = categories_data
+    categories.value[currentStep.value] = categories_data
+    nextTick(() => recordCategoryHeight())
 }
 
 function shouldCheckboxDisabled(checkboxValue: string, featureId: string, choose: number): boolean {
@@ -42,10 +53,20 @@ function shouldCheckboxDisabled(checkboxValue: string, featureId: string, choose
 
 function prevStep() {
     currentStep.value = Math.max(0, currentStep.value - 1)
+    for (let categoryName in categories.value[currentStep.value]) {
+        let category = categories.value[currentStep.value][categoryName]
+        category.collapse = false
+    }
+    nextTick(() => recordCategoryHeight())
 }
 
 function nextStep() {
     currentStep.value++
+    for (let categoryName in categories.value[currentStep.value]) {
+        let category = categories.value[currentStep.value][categoryName]
+        category.collapse = false
+    }
+    nextTick(() => recordCategoryHeight())
 }
 </script>
 <template>
@@ -77,17 +98,18 @@ function nextStep() {
         <div class="bg-slate-800 h-screen flex flex-col items-stretch">
             <h2 class="text-3xl mx-4 py-4 my-4 font-bold text-center border-b border-b-slate-50 flex-shrink-0">特质</h2>
             <div class="scroll-xs overflow-y-scroll m-4 h-64 flex-grow pr-4">
-                <div v-for="(features, categoryName) in categories" :key="categoryName">
+                <div v-for="(features, categoryName) in currentCategories" :key="categoryName">
                     <button
                         class="cursor-pointer hover:bg-slate-700 w-full text-left transition rounded-md p-2 mb-2 flex justify-between items-center"
                         @click="collapse(String(categoryName))">
                         <h3 class="text-2xl font-bold"> {{ categoryMapping[categoryName] }} </h3>
-                        <div class="relative w-4 h-1 transition" :class="{'rotate-45': !features.collapse}">
+                        <div class="relative w-4 h-1 transition" :class="{ 'rotate-45': !features.collapse }">
                             <div class="w-4 h-1 bg-slate-50"></div>
                             <div class="w-4 h-1 bg-slate-50 absolute top-0 transition rotate-90"></div>
                         </div>
                     </button>
-                    <div :class="{ collapsed: features.collapse }" class="overflow-hidden">
+                    <div class="collapse-container scroll-xs" :id="String(categoryName)" ref="categoryRefs"
+                        :class="{ collapsed: features.collapse }">
                         <div v-for="feature, idx in features.data" :key="idx" class="ml-4 mb-2">
                             <h3 class="font-bold text-lg">{{ feature.name }}</h3>
                             <p class="description" v-html="feature.description"></p>
@@ -121,7 +143,7 @@ main {
 }
 
 .collapsed {
-    @apply h-0;
+    height: 0 !important;
 }
 
 .step-circle {
@@ -148,6 +170,11 @@ input[type="checkbox"]:checked+div>.checkbox::after {
     bottom: 2px;
     right: 2px;
 }
+
+.collapse-container {
+    @apply overflow-hidden transition-all;
+}
+
 .checkbox-disabled {
     @apply cursor-not-allowed text-gray-400;
 }
