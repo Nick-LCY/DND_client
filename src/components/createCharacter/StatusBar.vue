411578d93d344c11d9ea27caad315cadf9c95f73
NickLin <nicklin9907@gmail.com>
2024-10-31 13:51:34 +0000

    First version of expression!

--- src/components/createCharacter/StatusBar.vue
+++ src/components/createCharacter/StatusBar.vue
@@ -1,37 +1,48 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-// import { computed, ref } from 'vue';
-// import { Effect as EffectType } from '../../assets/js/originalDataType';
+import { computed, ref, watch } from 'vue';
+import { Effect as EffectType } from '../../assets/js/originalDataType';
+import Character from '../../assets/js/context/character';
 
-// const props = defineProps<{ activatedEffects: EffectType[] }>()
-// const expressions = computed(
-//     () => props.activatedEffects.reduce(
-//         (prev: string[], current: EffectType): string[] => [...prev, ...current.expressions],
-//         []
-//     )
-// )
-interface Ability {
-    id: string,
-    name: string,
-    initValue: number,
-    saveProficiencies?: boolean
+const props = defineProps<{ activatedEffects: EffectType[] }>()
+const expressions = computed(
+    () => props.activatedEffects.reduce(
+        (prev: string[], current: EffectType): string[] => [...prev, ...current.expressions],
+        []
+    )
+)
+
+const character = ref(new Character())
+watch(expressions, () => {
+    character.value.reset()
+    function callInContext(js: string) {
+        return function (js: string) { return eval(js) }.call({ character: character.value }, js)
+    }
+    for (let expression of expressions.value) {
+        callInContext(expression)
+    }
+})
+
+const nameMapping = {
+    str: "力",
+    dex: "敏",
+    con: "体",
+    int: "智",
+    wis: "感",
+    cha: "魅",
 }
-const abilities = ref<Ability[]>([
-    { id: "str", name: "力", initValue: 8, saveProficiencies: true },
-    { id: "dex", name: "敏", initValue: 8 },
-    { id: "con", name: "体", initValue: 8, saveProficiencies: true },
-    { id: "int", name: "智", initValue: 8 },
-    { id: "wis", name: "感", initValue: 8 },
-    { id: "cha", name: "魅", initValue: 8 }
-])
+
 </script>
 <template>
     <div class="bg-slate-700 flex flex-col items-stretch justify-center px-1">
         <div class="text-center font-bold text-lg py-1">属性</div>
-        <div v-for="ability of abilities" class="text-center flex items-center justify-center border-t border-slate-50 py-2 last:border-b">
+        <div v-for="(value, ability) in character.abilities"
+            class="text-center flex items-center justify-center border-t border-slate-50 py-2 last:border-b">
             <div class="flex flex-col">
-                <span>{{ ability.name }}</span>
-                <span class="text-xl">{{ ability.initValue }}</span>
+                <div>
+                    <span>{{ nameMapping[ability] }}</span>
+                    <span v-if="character.saves[ability]">*</span>
+                </div>
+                <span class="text-xl">{{ value }}</span>
             </div>
         </div>
     </div>
